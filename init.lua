6ff0e1daeb29c23a6d34baddd63e5d06c2782380
Jesuseun Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2025-03-09 16:59:21 +0100

    install lualine

--- init.lua
+++ init.lua
@@ -54,8 +54,59 @@ require("lazy").setup({
         })
       end
     },
+    {
+      'nvim-lualine/lualine.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      config = function ()
+          require('lualine').setup ({
+            options = {
+              icons_enabled = true,
+              theme = 'auto',
+              component_separators = { left = '|', right = '|'},
+              section_separators = { left = '', right = ''},
+              disabled_filetypes = {
+                statusline = {},
+                winbar = {},
+              },
+              ignore_focus = {},
+              always_divide_middle = true,
+              always_show_tabline = true,
+              globalstatus = false,
+              refresh = {
+                statusline = 100,
+                tabline = 100,
+                winbar = 100,
+              }
+            },
+            sections = {
+              lualine_a = {'mode'},
+              lualine_b = {'diagnostics'},
+              lualine_c = {'filename'},
+              lualine_x = {'buffers'},
+              lualine_y = {},
+              lualine_z = {'location'}
+            },
+            inactive_sections = {
+              lualine_a = {},
+              lualine_b = {},
+              lualine_c = {'filename'},
+              lualine_x = {'location'},
+              lualine_y = {},
+              lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        })
 
+        require("lualine").setup({
+          options = { themes = require("catppuccin.utils.lualine") }
+        })
+        end
+    },
   },
+
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "catppuccin" } },
